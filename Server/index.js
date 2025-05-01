c879e3d0b052edcfc91134b30e6dbabb0e1215ea
SyedAbbas2001 <syedabbasofficial1@gmail.com>
2025-05-01 18:54:35 +0500

    updates

--- Server/index.js
+++ Server/index.js
@@ -106,6 +106,7 @@ app.delete('/users/:id', async (req, res) => {
   });
   
 
-app.listen(4000, () => {
-    console.log("Server is running on port 4000");
-});
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
